115484019e97b216037c85bfe2149f556cf0c5ac
Documenter.jl <documenter@juliadocs.github.io>
2024-07-26 17:38:58 +0000

    build based on 52579e9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ClaudePrompting","category":"page"},{"location":"#ClaudePrompting","page":"Home","title":"ClaudePrompting","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ClaudePrompting.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ClaudePrompting]","category":"page"}]
+}
