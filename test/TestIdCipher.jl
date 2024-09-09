e0084eb3be20ff54ba25794fe4fc44664328e76b
Kwang-Seuk Jeong <kjeong@gsdongju.ac.kr>
2024-09-09 09:13:16 +0900

    function names modified

--- test/TestIdCipher.jl
+++ test/TestIdCipher.jl
@@ -6,5 +6,5 @@ import ClaudePrompting.IdCipher as IC
 
 key = IC.generate_key()
 id = "2024194999"
-encrypted_id = IC.encrypt_id2(id, key)
-IC.decrypt_id2(encrypted_id, key)
+encrypted_id = IC.encrypt_id(id, key)
+IC.decrypt_id(encrypted_id, key)
