c5ff9039cf261785e44ad529f337451a7988b5f1
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-09-11 02:41:16 +0900

    dataframe conversion into yml format function added and tested

--- src/PrepareData.jl
+++ src/PrepareData.jl
@@ -3,9 +3,10 @@ module PrepareData
 using CSV
 using DataFrames
 using AES
+using OrderedCollections
 
 import ClaudePrompting.IdCipher as IC
-export encrypt_input_data
+export encrypt_input_data, df_to_yml
 
 function encrypt_input_data(df::DataFrame, ids_vec::Vector{String}, key::AES.AES128Key) 
   encrypted_ids = [IC.encrypt_id(id, key) for id in ids_vec]
@@ -15,6 +16,13 @@ function encrypt_input_data(df::DataFrame, ids_vec::Vector{String}, key::AES.AES
   return encrypted_df
 end
 
-
+function df_to_yml(df::DataFrame, key_order::Vector{Symbol})::Vector{Any}
+  yml_data = []
+  for row in eachrow(df) 
+    ordered_dict = OrderedDict(key => row[key] for key in key_order) 
+    push!(yml_data, ordered_dict)
+  end
+  return yml_data
+end
 
 end
