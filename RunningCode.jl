1d5eadf341c44a19ffb7f1b68351e6326459fdef
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-10-02 00:08:58 +0900

    codelines for IdAnonymizer added

--- RunningCode.jl
+++ RunningCode.jl
@@ -3,23 +3,26 @@ using YAML
 using PromptingTools
 using CSV
 using AES
+using JSON
 
 import ClaudePrompting.IdCipher as IC
 import ClaudePrompting.PrepareData as PD
 import ClaudePrompting.CommunicateClaude as CC
+import ClaudePrompting.IdAnonymizer as IA
 
 # 1. Prepare your data
 
 # rawdata
-csv_filepath = "/home/kjeong/localgit/ClaudePrompting.jl/week1_res.csv"
+csv_filepath = "/home/kjeong/localgit/ClaudePrompting.jl/week2_res.csv"
 # Claude 3 API
 api_filepath = "/home/kjeong/.config/anthropic/anthropic_api_key"
 # Prompt for Claude action
-prompt_filepath = "/home/kjeong/localgit/ClaudePrompting.jl/prompt"
-
-# 2. Data loading and encryption
+prompt_filepath = "/home/kjeong/localgit/ClaudePrompting.jl/prompt_W2"
 
 df = CSV.read(csv_filepath, DataFrame)
+
+# 2. Data loading and encryption or anonymization
+# 2.1. Encryption by AES
 ids_vec = string.(df[!, :IDs])
 key = IC.generate_key()
 encrypted_df = PD.encrypt_input_data(df, ids_vec, key)
@@ -33,10 +36,32 @@ loaded_key = open("aes_key.bin", "r") do io  # Load the AES key
   AES128Key(key_bytes)
 end
 
-# 3. Convert the DataFrame into YAML format
+# 2.2. Anonymization
+anonymized_df, id_map = IA.anonymize_ids(df)
 
-key_order = [:IDs, :Textdata]
-yml_data = PD.df_to_yml(encrypted_df, key_order)
+open("id_map.json", "w") do io  # Save the id_map to json file
+  JSON.print(io, id_map)
+end
+
+id_map_dict = open("id_map.json", "r") do io # Load the saved id_map
+  JSON.parse(io, dicttype=Dict{String, String})
+end
+Dict(parse(Int64, k) => v for (k, v) in id_map_dict)
+
+
+deanonymized_df = IA.deanonymize_ids(anonymized_df, id_map)
+df
+
+
+# 3. Convert the DataFrame into YAML format
+# Two ID processing methods are available, so you
+# need to choose the appropriate dataframe name for the
+# following process. For example, AES encryption would
+# use `encrypted_df` and anonymization would use
+# `anonymized_df`.
+
+key_order = [:IDs, :Ans]
+yml_data = PD.df_to_yml(anonymized_df, key_order)
 yml_string = YAML.yaml(yml_data)
 
 # 4. Setup Claude connection
@@ -46,7 +71,7 @@ batch_size = 10   # Set the number of data points to send to Claude at once
 prompt = read(prompt_filepath, String)
 responses = CC.process_sequential_batches(yml_string, prompt, batch_size)
 
-open("responses.txt", "w") do io   # Save responses into a file
+open("W2_responses1.txt", "w") do io   # Save responses into a file
   for (i, response) in enumerate(responses)
     write(io, response)
     if i < length(responses)
@@ -60,7 +85,10 @@ end
 
 # 5. Merge the responses into a single YAML file
 
-responses_load = YAML.load_file("responses.txt")
+responses_load = YAML.load_file("W2_responses.txt")
+
+
+
 all_keys = unique(vcat([collect(keys(response)) for response in responses_load]...))
 
 # 6. Create a DataFrame from the merged YAML data
@@ -72,6 +100,12 @@ df = DataFrame([key => Union{String, Missing}[] for key in column_order])
 for response in responses_load   # add each dict to the DataFrame
   row = Dict(key => get(response, key, missing) for key in column_order)
   push!(df, row)
+end
+
+
+deanonymized_df = IA.deanonymize_ids(df, id_map)
+open("commentary_w2.csv", "w") do io   # Save the comments into a CSV file
+  CSV.write(io, deanonymized_df)
 end
 
 # 7. Decrypt the IDs using the AES key
