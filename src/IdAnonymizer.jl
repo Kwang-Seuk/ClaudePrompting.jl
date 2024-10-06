4b6249b7223f27388043eb691a30a5b2d1157bb8
Kwang-Seuk <kjeong@bhug.ac.kr>
2024-10-06 22:28:42 +0900

    hash length increased for preventing duplication of anonymized ids

--- src/IdAnonymizer.jl
+++ src/IdAnonymizer.jl
@@ -13,20 +13,28 @@ end
 
 function anonymize_ids(df::DataFrame)::Tuple{DataFrame,Dict{Int64,String}}
   shuffled_df = df[shuffle(1:nrow(df)), :]
-
   n = nrow(shuffled_df)
   digits = get_id_digits(n)
 
-  id_map = Dict(
-    id => string("S", lpad((parse(Int, bytes2hex(sha256(string(id)))[1:6], base=16) % n) + 1, digits, '0'))
-    for id in shuffled_df.IDs
-  )
+  id_map = Dict{Int64,String}()
+  used_anonymous_ids = Set{String}()
+
+  for id in shuffled_df.IDs
+    while true
+      hash = bytes2hex(sha256(string(id)))
+      anonymous_id = "S" * hash[1:10]  # Use more characters for better uniqueness
+      if !(anonymous_id in used_anonymous_ids)
+        push!(used_anonymous_ids, anonymous_id)
+        id_map[id] = anonymous_id
+        break
+      end
+    end
+  end
 
   anonymized_df = DataFrame(
     IDs=[id_map[id] for id in shuffled_df.IDs],
     Ans=shuffled_df.Ans
   )
-
   return anonymized_df, id_map
 end
 
